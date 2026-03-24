30ba2408af3e7ef9c5f03507bb7cf1ec742cace7
Weston Houghton <whoughton@gmail.com>
2026-03-23 21:06:30 -0400

    fix(cli): decouple build from workspace source type graph

--- packages/cli/src/globals.d.ts
+++ packages/cli/src/globals.d.ts
@@ -64,3 +64,6 @@ declare module '@tachui/core/modifiers/type-generator' {
     conflicts: ModifierMetadataSnapshotConflict[]
   }
 }
+
+// This file needs to export something to be treated as a module
+export {}
